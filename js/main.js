9d348e8ed84b901c97ea1e42d871fb96e0e19de0
Ethan Shen <ethanshen.wu@gmail.com>
2019-08-04 15:59:19 -0700

    main.js: added Hero class and display

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+// extend Phaser.Sprite
+function Hero(game, x, y) {
+  Phaser.Sprite.call(this, game, x, y, 'hero');
+  // Phaser usually handles sprites by their center. 0 = left, down; 1 = right, up;
+  this.anchor.set(0.5, 0.5);
+};
+
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+
+Hero.prototype.constructer = Hero;
+
 PlayState = {};
 
 // load game assets
@@ -10,6 +21,7 @@ PlayState.preload = function () {
   this.game.load.image('grass:4x1', 'images/grass_4x1.png');
   this.game.load.image('grass:2x1', 'images/grass_2x1.png');
   this.game.load.image('grass:1x1', 'images/grass_1x1.png');
+  this.game.load.image('hero', 'images/hero_stopped.png');
 };
 
 // create game entities and set up world
@@ -21,11 +33,17 @@ PlayState.create = function () {
 PlayState._loadLevel = function (data) {
   //spawns all platforms
   data.platforms.forEach(this._spawnPlatform, this);
+  this._spawnCharacters({ hero: data.hero });
 };
 
 PlayState._spawnPlatform = function (platform) {
   this.game.add.sprite(platform.x, platform.y, platform.image);
 };
+
+PlayState._spawnCharacters = function (data) {
+  this.hero = new Hero(this.game, data.hero.x, data.hero.y);
+  this.game.add. existing(this.hero);
+}
 
 window.onload = function () {
     let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
